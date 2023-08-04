07ca494c4607c39e1490b33bcfc0eca235b02a86
Olya Zolotarchuk <zlotka1@gmail.com>
2023-08-04 09:26:32 +0200

    photo-web

--- photo-web/script.js
+++ photo-web/script.js
@@ -9,16 +9,6 @@ function documentClick(e) {
 
 
 
-function toggleHomeSection() {
-  var homeSection = document.getElementById("home");
-  homeSection.classList.toggle("hidden");
-}
-var menuIcon = document.querySelector(".icon-menu");
-menuIcon.addEventListener("click", function () {
-  toggleHomeSection();
-});
-
-
 // document.querySelector(".icon-menu").addEventListener("click", function () {
 //   // Toggle the "menu-open" class on the documentElement
 //   document.documentElement.classList.toggle("menu-open");
@@ -26,3 +16,16 @@ menuIcon.addEventListener("click", function () {
 //   // Toggle the "hidden" class on the home section
 //   document.getElementById("home").classList.toggle("hidden");
 // });
+
+
+function toggleSectionVisibility(sectionId) {
+  var section = document.getElementById(sectionId);
+  section.classList.toggle("hidden");
+}
+
+var menuIcon = document.querySelector(".icon-menu");
+menuIcon.addEventListener("click", function () {
+  toggleSectionVisibility("home"); 
+  toggleSectionVisibility("work");
+   toggleSectionVisibility("use");
+});
