be0199b8155ac448c3d40eca33ffc7c54934a31e
Olya Zolotarchuk <zlotka1@gmail.com>
2023-08-08 21:36:26 +0200

    >photo-web

--- photo-web/script.js
+++ photo-web/script.js
@@ -25,3 +25,33 @@ menuIcon.addEventListener("click", function () {
   toggleSectionVisibility("main");
   toggleSectionVisibility("footer");
 });
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+function handleImageAnimation() {
+  const animatedImages = document.querySelectorAll(".use__four .main__img");
+
+  animatedImages.forEach((image) => {
+    if (isInViewport(image)) {
+      image.classList.add("visible");
+    }
+  });
+}
+window.addEventListener("scroll", handleImageAnimation);
+handleImageAnimation();
+
+
+
+// ===========
+document.addEventListener("DOMContentLoaded", function () {
+  const homeSection = document.getElementById("home");
+  homeSection.classList.remove("hidden");
+});
